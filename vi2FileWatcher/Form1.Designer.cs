2492b8288866648395b403ce34b57cf5c60463ec
Stephan Jansky <stephan.jansky@vi2vi.com>
2021-07-06 14:33:45 +0200

    - initial version

---
+++ vi2FileWatcher/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace vi2FileWatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWatch = new System.Windows.Forms.Button();
+            this.txtFileToWatch = new System.Windows.Forms.TextBox();
+            this.lstLog = new System.Windows.Forms.ListBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtFilter = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chkInclSubDirs = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnWatch
+            // 
+            this.btnWatch.Location = new System.Drawing.Point(476, 38);
+            this.btnWatch.Name = "btnWatch";
+            this.btnWatch.Size = new System.Drawing.Size(75, 20);
+            this.btnWatch.TabIndex = 0;
+            this.btnWatch.Text = "Watch";
+            this.btnWatch.UseVisualStyleBackColor = true;
+            this.btnWatch.Click += new System.EventHandler(this.btnWatch_Click);
+            this.btnWatch.Leave += new System.EventHandler(this.btnWatch_Leave);
+            // 
+            // txtFileToWatch
+            // 
+            this.txtFileToWatch.Location = new System.Drawing.Point(12, 12);
+            this.txtFileToWatch.Name = "txtFileToWatch";
+            this.txtFileToWatch.ReadOnly = true;
+            this.txtFileToWatch.Size = new System.Drawing.Size(458, 20);
+            this.txtFileToWatch.TabIndex = 1;
+            this.txtFileToWatch.Text = "C:\\";
+            // 
+            // lstLog
+            // 
+            this.lstLog.FormattingEnabled = true;
+            this.lstLog.Location = new System.Drawing.Point(12, 64);
+            this.lstLog.Name = "lstLog";
+            this.lstLog.Size = new System.Drawing.Size(539, 381);
+            this.lstLog.TabIndex = 2;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(476, 12);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 20);
+            this.btnBrowse.TabIndex = 3;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtFilter
+            // 
+            this.txtFilter.Location = new System.Drawing.Point(312, 38);
+            this.txtFilter.Name = "txtFilter";
+            this.txtFilter.Size = new System.Drawing.Size(158, 20);
+            this.txtFilter.TabIndex = 4;
+            this.txtFilter.Text = "*.*";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(277, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Filter";
+            // 
+            // chkInclSubDirs
+            // 
+            this.chkInclSubDirs.AutoSize = true;
+            this.chkInclSubDirs.Location = new System.Drawing.Point(12, 38);
+            this.chkInclSubDirs.Name = "chkInclSubDirs";
+            this.chkInclSubDirs.Size = new System.Drawing.Size(131, 17);
+            this.chkInclSubDirs.TabIndex = 6;
+            this.chkInclSubDirs.Text = "Include Subdirectories";
+            this.chkInclSubDirs.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 450);
+            this.Controls.Add(this.chkInclSubDirs);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFilter);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.lstLog);
+            this.Controls.Add(this.txtFileToWatch);
+            this.Controls.Add(this.btnWatch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWatch;
+        private System.Windows.Forms.TextBox txtFileToWatch;
+        private System.Windows.Forms.ListBox lstLog;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtFilter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox chkInclSubDirs;
+    }
+}
+
---
+++ vi2FileWatcher/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vi2FileWatcher
+{
+    public partial class Form1 : Form
+    {
+        private FileSystemWatcher fs_Watcher = null;
+        public delegate void dWatcherNotificationEvent(string strMessage);
+        public event dWatcherNotificationEvent eWatcherNotificationEvent;
+        public StreamWriter sr_LogFile = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.eWatcherNotificationEvent += Form1_eWatcherNotificationEvent;
+        }
+
+        private void Form1_eWatcherNotificationEvent(string strMessage)
+        {
+            if (lstLog.InvokeRequired)
+            {
+                lstLog.Invoke(new MethodInvoker(() => { Form1_eWatcherNotificationEvent(strMessage); }));
+                return;
+            }
+
+            lstLog.Items.Add(strMessage);
+            sr_LogFile.WriteLine(strMessage);
+        }
+
+        private void Fs_Watcher_Error(object sender, ErrorEventArgs e)
+        {
+            this.eWatcherNotificationEvent(DateTime.Now.TimeOfDay + " - File Error: " + e.GetException().Message);
+        }
+
+        private void Fs_Watcher_Created(object sender, FileSystemEventArgs e)
+        {
+            FileInfo fsInfo = new FileInfo(e.FullPath);
+            this.eWatcherNotificationEvent(DateTime.Now.TimeOfDay + " - " + e.FullPath + " was CREATED!");
+            this.eWatcherNotificationEvent(DateTime.Now.TimeOfDay + " - " + e.FullPath + ": " + (fsInfo.Length > 1024 ? fsInfo.Length / 1024 + " KByte" : fsInfo.Length + " Byte"));
+        }
+
+        private void Fs_Watcher_Deleted(object sender, FileSystemEventArgs e)
+        {
+            FileInfo fsInfo = new FileInfo(e.FullPath);
+            this.eWatcherNotificationEvent(DateTime.Now.TimeOfDay + " - " + e.FullPath + " was DELETED!");
+            this.eWatcherNotificationEvent(DateTime.Now.TimeOfDay + " - " + e.FullPath + ": " + (fsInfo.Length > 1024 ? fsInfo.Length / 1024 + " KByte" : fsInfo.Length + " Byte"));
+        }
+
+        private void Fs_Watcher_Renamed(object sender, RenamedEventArgs e)
+        {
+            FileInfo fsInfo = new FileInfo(e.FullPath);
+            this.eWatcherNotificationEvent(DateTime.Now.TimeOfDay + " - " + e.FullPath + " was RENAMED!");
+            this.eWatcherNotificationEvent(DateTime.Now.TimeOfDay + " - " + e.FullPath + ": " + (fsInfo.Length > 1024 ? fsInfo.Length / 1024 + " KByte" : fsInfo.Length + " Byte"));
+        }
+
+        private void Fs_Watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            FileInfo fsInfo = new FileInfo(e.FullPath);
+            this.eWatcherNotificationEvent(DateTime.Now.TimeOfDay + " - " + e.FullPath + " was CHANGED!");
+            this.eWatcherNotificationEvent(DateTime.Now.TimeOfDay + " - " + e.FullPath + ": " + (fsInfo.Length > 1024 ? fsInfo.Length / 1024 + " KByte" : fsInfo.Length + " Byte"));
+        }
+
+        private void btnWatch_Click(object sender, EventArgs e)
+        {
+            if (btnWatch.Text == "Watch")
+            {
+                if (String.IsNullOrEmpty(txtFileToWatch.Text))
+                {
+                    lstLog.Items.Add("File to watch is EMPTY!");
+                    return;
+                }
+                sr_LogFile = new StreamWriter("vi2FileWatcher.log");
+                sr_LogFile.WriteLine("***Start: " + DateTime.Now + "***");
+
+                foreach (string strFile in System.IO.Directory.GetFiles(txtFileToWatch.Text) )
+                {
+                    FileInfo fsInfo = new FileInfo(strFile);
+                    this.eWatcherNotificationEvent(strFile + ": " + (fsInfo.Length > 1024 ? fsInfo.Length / 1024 + " KByte" : fsInfo.Length + " Byte"));
+                    lstLog.Items.Add(strFile + ": " + (fsInfo.Length > 1024 ? fsInfo.Length / 1024 + " KByte" : fsInfo.Length + " Byte"));
+                }
+
+                fs_Watcher = new FileSystemWatcher(txtFileToWatch.Text);
+                fs_Watcher.NotifyFilter = NotifyFilters.Attributes | NotifyFilters.CreationTime | NotifyFilters.DirectoryName | NotifyFilters.FileName | NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.Security
+                                        | NotifyFilters.Size;
+                fs_Watcher.IncludeSubdirectories = chkInclSubDirs.Checked;
+                fs_Watcher.EnableRaisingEvents = true;
+                fs_Watcher.Filter = txtFilter.Text;
+                fs_Watcher.Changed += Fs_Watcher_Changed;
+                fs_Watcher.Renamed += Fs_Watcher_Renamed;
+                fs_Watcher.Deleted += Fs_Watcher_Deleted;
+                fs_Watcher.Created += Fs_Watcher_Created;
+                fs_Watcher.Error += Fs_Watcher_Error;
+
+                if (chkInclSubDirs.Checked)
+                {
+                    this.eWatcherNotificationEvent("Watching Directory and Subdirectories with Filter (" + txtFilter.Text + "): " + txtFileToWatch.Text);
+                    lstLog.Items.Add("Watching Directory and Subdirectories with Filter (" + txtFilter.Text + "): " + txtFileToWatch.Text);
+                }
+                else
+                {
+                    this.eWatcherNotificationEvent("Watching Directory with Filter (" + txtFilter.Text + "): " + txtFileToWatch.Text);
+                    lstLog.Items.Add("Watching Directory with Filter (" + txtFilter.Text + "): " + txtFileToWatch.Text);
+                }
+                btnWatch.Text = "STOP";
+            }
+            else
+            {
+                fs_Watcher.Dispose();
+                fs_Watcher = null;
+                btnWatch.Text = "Watch";
+                lstLog.Items.Add("Stopped watching...");
+                sr_LogFile.WriteLine("***END: " + DateTime.Now + "***");
+                sr_LogFile.Close();
+                sr_LogFile = null;
+            }
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            DialogResult drBrowse = folderBrowserDialog1.ShowDialog();
+            if (drBrowse != DialogResult.OK)
+                return;
+
+            txtFileToWatch.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void btnWatch_Leave(object sender, EventArgs e)
+        {
+            if ( sr_LogFile != null )
+                sr_LogFile.WriteLine("***END: " + DateTime.Now + "***");
+        }
+    }
+}
